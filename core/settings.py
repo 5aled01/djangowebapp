e25b590ada8cc994db54b268123eac1c5688e18a
Khaled 01 <5aledouldmed@gmail.com>
2023-07-02 18:56:24 +0100

    modify ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'web-production-ce34.up.railway.app']
+ALLOWED_HOSTS = ['web-production-ce34.up.railway.app', 'localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
