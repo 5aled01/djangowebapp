ff429d6e4d71328476bd4955b0df78e657bdc446
Khaled 01 <5aledouldmed@gmail.com>
2023-07-02 18:54:01 +0100

    add ALLOWED_HOSTS for railway

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'web-production-ce34.up.railway.app']
 
 # Application definition
 
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '0vGJCdXmatQ2ItDZC4Xc',
+        'HOST': 'containers-us-west-94.railway.app',
+        'PORT': '6760',
     }
 }
 
